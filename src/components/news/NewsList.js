771ef886550c1478ceb8a6170b052159a21ac61e
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-03-06 13:17:32 -0600

    working articles displaying/routing

---
+++ src/components/news/NewsList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+import "./news.css"
+
+
+export default class NewsList extends Component {
+    render() {
+        return (
+            <section className="news">
+                { this.props.articles.filter(article => article.userId === Number(sessionStorage.getItem("credentials")))
+                    .map( article =>
+                        <div key={article.id} className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    {article.news}
+                                    <br></br>
+                                </h5>
+                                <div> Synopsis: {article.newsSynopsis}</div>
+                            </div>
+                        </div>
+                    )
+                }
+            </section>
+        )
+    }
+}
